b87b6dbf2475b7dcb395510782c3ce507e85180c
Mohd Monish <surties.it@gmail.com>
2024-03-30 16:06:04 +0530

    update

--- src/Pages/DetailNews.jsx
+++ src/Pages/DetailNews.jsx
@@ -59,27 +59,74 @@ function DetailNews() {
     <div>
       <Helmet>
         <title>{articleData.heading}</title>
-        <meta name="title" content={articleData.heading} />
-        <meta name="description" content={articleData.article} />
+        <meta
+          name="news_keywords"
+          content={articleData.heading}
+          itemprop="keywords"
+        />
+        <meta
+          name="description"
+          content={articleData.article}
+          itemprop="description"
+        />
+        <meta name="section" content="surat" itemprop="articleSection" />
+        <meta
+          name="url"
+          content={`https://surties.in/news/id/${articleData._id}`}
+          itemprop="url"
+        />
+        <link href={articleData.heading} rel="amphtml" />
+        <link href="https://surties.in" rel="publisher" />
 
-        <meta property="og:type" content="website" />
+        <meta name="keywords" content={articleData.heading} />
+        <meta name="robots" content="max-image-preview:large" />
+        <meta property="og:title" content={articleData.subHeading} />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content={articleData.thumbnail} />
+        <meta property="og:image:secure_url" content={articleData.thumbnail} />
+        <meta property="og:image:width" content="630" />
+        <meta property="og:image:height" content="473" />
         <meta
           property="og:url"
-          content={`https://surtie.in/news/${articleData._id}`}
+          content={`https://surties.in/news/id/${articleData._id}`}
         />
-        <meta property="og:title" content={articleData.heading} />
-        <meta property="og:description" content={articleData.article} />
-        <meta property="og:image" content={articleData.thumbnail} />
+        <meta property="og:site_name" content="surties.in" />
+        <meta property="og:description" content={articleData.heading} />
+        <meta property="fb:app_id" content="213741912058651" />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content={`https://surtie.in/news/${articleData._id}`}
+        <meta property="al:android:app_name" content="surties" />
+
+        <meta property="al:ios:app_store_id" content="377869410" />
+        <meta property="al:ios:app_name" content="surties" />
+
+        <meta property="al:ipad:app_store_id" content="390847563" />
+        <meta property="al:ipad:app_name" content="Surties" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@surties" />
+        <meta name="twitter:url" content={articleData.heading} />
+        <meta name="twitter:title" content={articleData.heading} />
+        <meta name="twitter:description" content={articleData.heading} />
+        <meta name="twitter:image" content={articleData.thumbnail} />
+        <meta name="twitter:app:url:iphone" content="" />
+        <meta name="twitter:app:url:googleplay" content="" />
+        <meta property="snapchat:sticker" content={articleData.thumbnail} />
+        <link rel="preload" as="image" href={articleData.thumbnail} />
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="taboola-category" content="news" />
+
+        <meta property="taboola:title" content={articleData.heading} />
+
+        <link rel="canonical" href={articleData.heading} />
+        <link
+          itemprop="mainEntityOfPage"
+          href={`https://surties.in/news/id/${articleData._id}`}
         />
-        <meta property="twitter:title" content={articleData.heading} />
-        <meta property="twitter:description" content={articleData.article} />
-        <meta property="twitter:image" content={articleData.thumbnail} />
-      </Helmet>
+        <link
+          rel="apple-touch-icon"
+          href="https://firebasestorage.googleapis.com/v0/b/surtieswebapplication.appspot.com/o/Surties%20Zomato%20Red%20Logo%203.png?alt=media&token=a7f9e6f9-4eb3-4b65-8134-9fd72fc6f3ed"
+        />
+      </Helmet>{" "}
       {loading ? (
         <Center mt={"20px"}>
           <Spinner
